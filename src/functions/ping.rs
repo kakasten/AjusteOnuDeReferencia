81e0a1aeda57149744f7d5c83001fadc42930129
Luiz Fernando <kakasten.cipher@gmail.com>
2024-05-17 20:59:17 -0300

    Refactored function

--- src/functions/ping.rs
+++ src/functions/ping.rs
@@ -1,19 +1,22 @@
 // ping command
 
 use std::process::{Command, Output};
+use std::io::{self, Result};
 
-pub fn ping( ip: &String) -> Output{
-    let output: Output = if cfg!(target_os = "windows") {
+pub fn ping(ip: &String) -> Result<Output> {
+    let output = if cfg!(target_os = "windows") {
         Command::new("cmd")
             .args(&["/C", "ping", "-n", "2", ip])
-            .output()
-            .expect("Falha ao executar o comando")
+            .output()?
     } else {
         Command::new("ping")
             .args(&["-c", "2", ip])
-            .output()
-            .expect("Falha ao executar o comando")
+            .output()?
     };
-    outputs
-}
 
+    if output.status.success() {
+        Ok(output)
+    } else {
+        Err(io::Error::new(io::ErrorKind::Other, "Comando ping falhou"))
+    }
+}
