48093888d16708a794ce779099a8311d5c5e9e55
Luiz Fernando <kakasten.cipher@gmail.com>
2024-05-17 21:04:43 -0300

    Refactored function

--- src/functions/ssh_connection.rs
+++ src/functions/ssh_connection.rs
@@ -1,37 +1,32 @@
-use std::{io::Read, net::TcpStream, sync::mpsc::channel};
+#[path = "../domain/constants.rs"]
+mod constants;
+
+use std::{io::Read, net::TcpStream};
 use ssh2::{Channel, Session};
 
-const PORT:u16 = 22;
-
-pub fn ssh_connection( ip: String, user: &String, password: &String, mut session: Session) {
-    match TcpStream::connect ((ip, PORT)) {
-        Ok (_tcp) => {
+pub fn ssh_connection(ip: &str, user: &str, password: &str, session: &mut Session) {
+    match TcpStream::connect((ip, constants::PORT)) {
+        Ok(tcp) => {
+            session.set_tcp_stream(tcp);
             session.handshake().expect("Falha no handshake");
-            session.userauth_password(&user, &password).expect("Falha na autenticação");
-
-            if session.authenticated() {
-                ssh_open_channel(session);
-            } else {
-                eprintln!("Falha na autenticação");
-            }
+            session.userauth_password(user, password).expect("Falha na autenticação");
         },
-        Err( e ) => eprintln!("Falha ao conectar ao servidor ssh: {}", e),
+        Err(e) => eprintln!("Falha ao conectar ao servidor ssh: {}", e),
     }
 }
 
-pub fn ssh_open_channel( session: Session) -> Channel{
-    let mut channel = session.channel_session().expect("Falha ao abrir canal");
-    channel
+pub fn ssh_open_channel(session: &mut Session) -> Channel {
+    session.channel_session().expect("Falha ao abrir canal")
 }
 
-pub fn ssh_closed ( mut channel: Channel) {
+pub fn ssh_close(channel: &mut Channel) {
     channel.send_eof().expect("Falha ao enviar EOF");
     channel.wait_close().expect("Falha ao fechar o canal");
 }
 
-pub fn ssh_command (mut channel: Channel, command: &String) {
-    channel.exec(&command).expect("Falha ao executar o comando");
+pub fn ssh_command(channel: &mut Channel, command: &str) {
+    channel.exec(command).expect("Falha ao executar o comando");
     let mut s = String::new();
-    channel.read_to_string(&mut s).expect("Falha ao ler saida do comando");
+    channel.read_to_string(&mut s).expect("Falha ao ler saída do comando");
     println!("{}", s);
 }
