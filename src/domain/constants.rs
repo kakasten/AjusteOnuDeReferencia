84c3b55220c851fbaf5f41cda708ca0ed3668f16
Luiz Fernando <kakasten.cipher@gmail.com>
2024-05-21 12:37:17 -0300

    Adding more constants

--- src/domain/constants.rs
+++ src/domain/constants.rs
@@ -1,7 +1,9 @@
 //Varieties constants
+use std::time::Duration;
 
 pub const PORT:u16 = 22;
 pub const IP: &str = "192.168.1.1";
 pub const USER_NAME: &str = "admin";
 pub const PASSWORD: &str = "intelbras";
-pub const MULTIPLE_REGISTER: &[i16] = &[0x90, 0xd0, 0x70];
+pub const MULTIPLE_REGISTER: &[i32] = &[0x90, 0xd0, 0x70];
+pub const TIME_OUT: std::time::Duration  = Duration::from_millis(650);
